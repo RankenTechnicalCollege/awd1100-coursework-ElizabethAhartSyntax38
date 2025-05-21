0f70c725d151776138f05f9ba71e0b32dc404c1c
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-05-21 12:18:00 -0500

    completed lab1 ch02-03

---
+++ Lab Assignments/CH02/Ch02-03/Lab1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("please enter a number of miles");
+double numMiles = Convert.ToDouble(Console.ReadLine());
+double numKilometers = numMiles * 1.6;
+Console.WriteLine($"{numMiles} miles is {numKilometers} kilometers.");
