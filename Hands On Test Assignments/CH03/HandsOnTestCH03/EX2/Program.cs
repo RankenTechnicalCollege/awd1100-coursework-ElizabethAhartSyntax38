2a81b367880b62d84fbbf4905b99d77f0ecb1986
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-05-22 20:28:58 -0500

    HandsOntestCH03
    EX1EX2EX3

---
+++ Hands On Test Assignments/CH03/HandsOnTestCH03/EX2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write an Age");
+int age=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write a second Age!");
+int age2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Write a third RANDOM Age!");
+int age3=Convert.ToInt32(Console.ReadLine());
+Double Average = (age + age2 + age3*.03);
+Console.WriteLine("Average of Age is {0}",Average);
---
+++ Hands On Test Assignments/CH03/HandsOnTestCH03/EX3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your Name?");
+string name =Console.ReadLine();
+Console.WriteLine("What is your Address?");
+string Address=Console.ReadLine();
+Console.WriteLine("what is your City,State,and Zip?");
+string cityStateZip=Console.ReadLine();
+Console.WriteLine("How many T-shirts Purchased?");
+string num = Console.ReadLine();
+const float TAXRATE = 0.8f;
+double num2 =Convert.ToDouble(num) * (14.99)*(TAXRATE);
+Console.WriteLine("Total Price is {0:C}",num2);
+
+Console.WriteLine("Plus tax 0.8 Amount Due is (salesTax)+(total Price)");
+
+
