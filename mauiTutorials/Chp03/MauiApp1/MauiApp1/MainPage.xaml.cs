4d2db60e1a5b1447a0d154b94ed28e0992b7cafd
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-05-28 23:51:39 -0500

    maui Calc Retry
    First Maui Retry

---
+++ mauiTutorials/Chp03/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            // count++;
+
+            // if (count == 1)
+            // CounterBtn.Text = $"Clicked {count} time";
+            // else
+            // CounterBtn.Text = $"Clicked {count} times";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+            //Get input from the User.
+            string enterGrade = TxtNumberGrade.Text;
+            int numberGrade;
+            int.TryParse(enterGrade, out numberGrade);
+            if (numberGrade == 0)
+            {
+                lblLetterGrade.Text = "Please Enter a valid number 1-100 ";
+            } else if (numberGrade < 0 || numberGrade > 100)
+            {
+
+
+                //Declare the Grade using if else  - if block
+
+                //Output The Letter
+            }
+
+        }
+    
