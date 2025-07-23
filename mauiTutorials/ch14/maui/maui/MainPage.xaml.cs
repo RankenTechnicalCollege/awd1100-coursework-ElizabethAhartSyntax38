890c8331cd8a04103c864d21c58d2093d2ea36b7
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-07-23 12:04:50 -0500

    Maui Tutorial CH 14/15

---
+++ mauiTutorials/ch14/maui/maui/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿
+using maui.Models;
+using System.Collections.ObjectModel;
+
+
+
+namespace maui
+{
+    public partial class MainPage : ContentPage
+    {
+       public ObservableCollection<InventoryItem> InventoryItems { get; set; }= [];
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InventoryItems.Add(new InventoryItem(1000, "Laptop", 999.99m));
+            InventoryItems.Add(new InventoryItem(2000, "Mouse", 19.99m));
+            BindingContext= this;
+        }
+
+        private void btnAdd_Clicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(ItemNOEntry.Text, out int itemNo)&&
+                !string.IsNullOrEmpty(DescriptionEntry.Text)&&
+                decimal.TryParse(PriceEntry.Text,out decimal price))
+            {
+                InventoryItems.Add(new InventoryItem(itemNo, DescriptionEntry.Text, price));
+                ClearEntries();
+            }
+            else
+            {
+                DisplayAlert("Error", "Please enter Valid values for all feilds.", "ok");
+
+            }
+        }
+
+        private void btnUpdate_Clicked(object sender, EventArgs e)
+        {
+            if (InventoryListView.SelectedItem is InventoryItem selectedItem)
+            {
+                if (int.TryParse(ItemNOEntry.Text, out int itemNo) &&
+                !string.IsNullOrEmpty(DescriptionEntry.Text) &&
+                 decimal.TryParse(PriceEntry.Text, out decimal price))
+                {
+                    selectedItem.ItemNo = itemNo;
+                    selectedItem.Description = DescriptionEntry.Text;
+                    selectedItem.Price = price;
+
+                   // InventoryItems[InventoryItems.IndexOf(selectedItem)] = selectedItem;
+                    //ClearEntries();
+
+                }
+
+                else
+                {
+
+                }
+
+
+
+
+            }
+        }
+
+        private void btnDelete_Clicked(object sender, EventArgs e)
+        {
+            if (InventoryListView.SelectedItem is InventoryItem selectionItem)
+            { 
+                InventoryItems.Remove(selectionItem);
+                ClearEntries();
+            }
+
+        }
+
+        private void InventoryListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem is InventoryItem selectedItem)
+            {
+                ItemNOEntry.Text = selectedItem.ItemNo.ToString();
+                DescriptionEntry.Text = selectedItem.Description;
+                PriceEntry.Text = selectedItem.Price.ToString("F2");
+            }
+        }
+        private void ClearEntries()
+        {
+            ItemNOEntry.Text = string.Empty;
+            DescriptionEntry.Text = string.Empty;
+            PriceEntry.Text = string.Empty;
+        }
+    }
+}
---
+++ mauiTutorials/ch14/maui/maui/Models/InventoryItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maui.Models
+{
+    public class InventoryItem(int itemNo, string description, decimal price) : INotifyPropertyChanged
+    {
+        //public int ItemNo { get; set; }
+        //public string Description { get; set; }
+        //public decimal Price { get; set; }
+        private int _itemNo= itemNo;
+        private string _description= description;
+        private decimal _price= price;
+
+        public int ItemNo
+        {
+            get => _itemNo;
+            set 
+            { 
+                _itemNo = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("itemNo"));
+                
+            }
+        }
+        public string Description
+        {
+            get =>_description;
+            set
+            {
+                _description = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Description"));
+
+            }
+        }
+        public decimal Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Price"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PriceDisplay"));
+
+            }
+        }
+        public string PriceDisplay=>_price.ToString("c");
+
+        //public InventoryItem(int itemNo,string description, decimal price) 
+        //{
+        //    _itemNo = itemNo;
+        //    _description = description;
+        //    _price = price;
+
+        //}
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
